c6d12652792bdd241e2f5f65e97a8c83956741b6
David Paez <dpaez013@fiu.edu>
2022-03-30 00:21:43 -0700

    Added ping logic and speedometer UI

--- Apex Legends Pinger/ContentView.swift
+++ Apex Legends Pinger/ContentView.swift
@@ -9,18 +9,134 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var hostName = "Hello World!"
+    @State var durationText = "Duration: "
+    @State var progress : CGFloat = 0
+    let colors = [Color("Aqua"), Color("Teal")]
+    
     var body: some View {
-        Text("Hello!")
-        Button("Click Here") {
-            print("Hello World!")
+        
+        VStack()
+        {
+            Text(hostName)
+            Button("Click Here", action: doSomething)
+            Text(durationText)
         }
+        .padding(.bottom, 50)
+        
+        VStack
+        {
+            Meter(progress: self.$progress)
+            
+            HStack(spacing: 25){
+                
+                Button(action: {
+                    
+                    withAnimation(Animation.default.speed(0.55)){
+                        
+                        self.progress += 10
+                        
+                    }
+                    
+                }) {
+                    
+                    Text("Update")
+                        .padding(.vertical,10)
+                        .frame(width: (UIScreen.main.bounds.width - 50) / 2)
+                    
+                }
+                .background(Capsule().stroke(LinearGradient(gradient: .init(colors: self.colors), startPoint: .leading, endPoint: .trailing), lineWidth: 2))
+                
+                
+                Button(action: {
+                    
+                    withAnimation(Animation.default.speed(0.55)){
+                        
+                        self.progress = 0
+                    }
+                    
+                }) {
+                    
+                    Text("Reset")
+                        .padding(.vertical,10)
+                        .frame(width: (UIScreen.main.bounds.width - 50) / 2)
+                    
+                }
+                .background(Capsule().stroke(LinearGradient(gradient: .init(colors: self.colors), startPoint: .leading, endPoint: .trailing), lineWidth: 2))
+            }
+            .padding(.top, 55)
+        }
+    }
+    
+    func doSomething()
+    {
+        print("Hello David!");
+        hostName = "www.google.com";
+        
+        // Ping once
+        let once = try? SwiftyPing(host: hostName, configuration: PingConfiguration(interval: 0.2, with: 5), queue: DispatchQueue.global())
+        once?.observer = { (response) in
+            let durationInMs = response.duration * 1000
+            print(durationInMs)
+            durationText = "Duration: " + String(durationInMs)
+            withAnimation(Animation.default.speed(0.15))
+            {
+                self.progress = durationInMs
+            }
+        }
+        once?.targetCount = 100
+        try? once?.startPinging()
+    }
+}
+
+struct Meter: View
+{
+    let colors = [Color("Aqua"), Color("Teal")]
+    @Binding var progress : CGFloat
+    
+    var body: some View {
+         ZStack()
+        {
+            ZStack()
+            {
+                Circle()
+                    .trim(from: 0, to: 0.5)
+                    .stroke(Color.black.opacity((0.1)), lineWidth: 55)
+                    .frame(width: 280, height: 280)
+                
+                Circle()
+                    .trim(from: 0, to: self.setProgress())
+                    .stroke(AngularGradient(gradient:.init(colors:self.colors), center:.center, angle: .init(degrees: 180)), lineWidth: 55)
+                    .frame(width: 280, height: 280)
+            }
+            .rotationEffect(.init(degrees: 180))
+            
+            ZStack(alignment: .bottom) {
+                
+                self.colors[0]
+                .frame(width: 2, height: 95)
+                
+                Circle()
+                    .fill(self.colors[0])
+                    .frame(width: 15, height: 15)
+            }
+            .offset(y: -35)
+            .rotationEffect(.init(degrees: -90))
+            .rotationEffect(.init(degrees: self.setArrow()))
+        }
+        .padding(.bottom, -20)
+    }
+    
+    func setProgress()->CGFloat{
+        
+        let temp = self.progress / 2
+        return temp * 0.01
+    }
+    
+    func setArrow()->Double{
+        
+        let temp = self.progress / 100
+        return Double(temp * 180)
     }
 }
 
